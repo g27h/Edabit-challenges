7a690b1c59b23378485bb8d0f5034a5d9ddf72f2
g27h <gus27h@vt.edu>
2020-03-04 20:11:43 -0500

    Initial commit for the edabit challenges project to desktop app
    Allows us to be able to directly commit without having to go to the website!
    
    -We had to create a completely new project and could not copy a project over bc the folder name was different.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/** Quick things learned:
+ * - To create a project in Visual Studio Code: use "dotnet new console" to create a new console application
+ * - then run "dotnet build" to build the project, and "dotnet restore"
+ * */
+namespace Edabit_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
